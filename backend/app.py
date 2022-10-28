becc66b5f5ce3a20216497fd7e7298955286bdb5
Black-Lotus-1207 <sayalipatil1207@gmail.com>
2022-10-28 19:26:39 +0530

    live streaming

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask,render_template,Response
+import cv2
+
+app=Flask(__name__)
+camera=cv2.VideoCapture(0)
+
+def generate_frames(id):
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read(id)
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/video/<int:id>')
+def video(id):
+    return Response(generate_frames(id),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-#hello world
+if __name__=="__main__":
+    app.run(debug=True)
